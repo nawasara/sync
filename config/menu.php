b044d4f084f7dcc72f53d8d00f1175e2bf992474
Pringgo J. Saputro <odyinggo@gmail.com>
2026-06-28 07:29:00 +0700

    chore(menu): assign workspace group + standardize Indonesian label

--- config/menu.php
+++ config/menu.php
@@ -7,6 +7,7 @@ return [
         'workspace' => 'monitoring',
         'label' => 'Monitoring',
         'icon' => 'lucide-activity',
+        'group' => 'Observability',
         'url' => '',
         'permission' => 'sync.job.view',
         'submenu' => [
